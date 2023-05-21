346e28a52e476dc69e3a2f7ed7e71c5f49e082f0
AlexBrodbelt <apbrodbelt@gmail.com>
2023-05-21 17:51:05 +0100

    deleted unnecessary boilerplate code

--- src/systems.rs
+++ src/systems.rs
@@ -102,136 +102,10 @@ pub fn setup(
     commands.spawn(WallBundle::new(WallLocation::Top));
 
     // Cuestick-Player
-    
-
-    // // Bricks
-    // // Negative scales result in flipped sprites / meshes,
-    // // which is definitely not what we want here
-    // assert!(BRICK_SIZE.x > 0.0);
-    // assert!(BRICK_SIZE.y > 0.0);
-
-    // let total_width_of_bricks = (RIGHT_WALL - LEFT_WALL) - 2. * GAP_BETWEEN_BRICKS_AND_SIDES;
-    // let bottom_edge_of_bricks = paddle_y + GAP_BETWEEN_PADDLE_AND_BRICKS;
-    // let total_height_of_bricks = TOP_WALL - bottom_edge_of_bricks - GAP_BETWEEN_BRICKS_AND_CEILING;
-
-    // assert!(total_width_of_bricks > 0.0);
-    // assert!(total_height_of_bricks > 0.0);
-
-    // // Given the space available, compute how many rows and columns of bricks we can fit
-    // let n_columns = (total_width_of_bricks / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)).floor() as usize;
-    // let n_rows = (total_height_of_bricks / (BRICK_SIZE.y + GAP_BETWEEN_BRICKS)).floor() as usize;
-    // let n_vertical_gaps = n_columns - 1;
-
-    // // Because we need to round the number of columns,
-    // // the space on the top and sides of the bricks only captures a lower bound, not an exact value
-    // let center_of_bricks = (LEFT_WALL + RIGHT_WALL) / 2.0;
-    // let left_edge_of_bricks = center_of_bricks
-    //     // Space taken up by the bricks
-    //     - (n_columns as f32 / 2.0 * BRICK_SIZE.x)
-    //     // Space taken up by the gaps
-    //     - n_vertical_gaps as f32 / 2.0 * GAP_BETWEEN_BRICKS;
-
-    // // In Bevy, the `translation` of an entity describes the center point,
-    // // not its bottom-left corner
-    // let offset_x = left_edge_of_bricks + BRICK_SIZE.x / 2.;
-    // let offset_y = bottom_edge_of_bricks + BRICK_SIZE.y / 2.;
-
-    // for row in 0..n_rows {
-    //     for column in 0..n_columns {
-    //         let brick_position = Vec2::new(
-    //             offset_x + column as f32 * (BRICK_SIZE.x + GAP_BETWEEN_BRICKS),
-    //             offset_y + row as f32 * (BRICK_SIZE.y + GAP_BETWEEN_BRICKS),
-    //         );
-
-    //         // brick
-    //         commands.spawn((
-    //             SpriteBundle {
-    //                 sprite: Sprite {
-    //                     color: BRICK_COLOR,
-    //                     ..default()
-    //                 },
-    //                 transform: Transform {
-    //                     translation: brick_position.extend(0.0),
-    //                     scale: Vec3::new(BRICK_SIZE.x, BRICK_SIZE.y, 1.0),
-    //                     ..default()
-    //                 },
-    //                 ..default()
-    //             },
-    //             Brick,
-    //             Collider,
-    //         ));
-    //     }
-    // }
 }
 
-// pub fn check_for_collisions(
-//     mut commands: Commands,
-//     mut scoreboard: ResMut<Scoreboard>,
-//     mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
-//     collider_query: Query<(Entity, &Transform, Option<&Brick>), With<Collider>>,
-//     mut collision_events: EventWriter<CollisionEvent>,
-// ) { 
-//     for (mut ball_velocity, ball_transform) in &mut ball_query {
-        
-//         // let (mut ball_velocity, ball_transform) = ball_query.single_mut();
-//         let ball_size = ball_transform.scale.truncate();
 
-//         // check collision with walls
-//         for (collider_entity, transform, maybe_brick) in &collider_query {
-//             let collision = collide(
-//                 ball_transform.translation,
-//                 ball_size,
-//                 transform.translation,
-//                 transform.scale.truncate(),
-//             );
-//             if let Some(collision) = collision {
-//                 // Sends a collision event so that other systems can react to the collision
-//                 collision_events.send_default();
 
-//                 // Bricks should be despawned and increment the scoreboard on collision
-//                 if maybe_brick.is_some() {
-//                     scoreboard.score += 1;
-//                     commands.entity(collider_entity).despawn();
-//                 }
-
-//                 // reflect the ball when it collides
-//                 let mut reflect_x = false;
-//                 let mut reflect_y = false;
-
-//                 // only reflect if the ball's velocity is going in the opposite direction of the
-//                 // collision
-//                 match collision {
-//                     Collision::Left => reflect_x = ball_velocity.x > 0.0,
-//                     Collision::Right => reflect_x = ball_velocity.x < 0.0,
-//                     Collision::Top => reflect_y = ball_velocity.y < 0.0,
-//                     Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
-//                     Collision::Inside => { /* do nothing */ }
-//                 }
-
-//                 // reflect velocity on the x-axis if we hit something on the x-axis
-//                 if reflect_x {
-//                     ball_velocity.x = -ball_velocity.x;
-//                 }
-
-//                 // reflect velocity on the y-axis if we hit something on the y-axis
-//                 if reflect_y {
-//                     ball_velocity.y = -ball_velocity.y;
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// pub fn velocity_system(mut query: Query<&mut Velocity>) {
-//     for mut velocity in &mut query{
-//         velocity.x -= velocity.x * FRICTION_COEFFICIENT * TIME_STEP;
-//         velocity.y -= velocity.y * FRICTION_COEFFICIENT * TIME_STEP;
-
-//         if velocity.length_squared() < STOPPING_THRESHOLD {
-//             *velocity = Velocity(Vec2::ZERO);
-//         }
-//     } 
-// }
 
 // pub fn move_system(mut query: Query<(&mut Transform, &Velocity)>) {
 //     for (mut transform, velocity) in &mut query {
@@ -283,9 +157,3 @@ pub fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text
     let mut text = query.single_mut();
     text.sections[1].value = scoreboard.score.to_string();
 }
-
-// pub fn collision_detection_between_balls(a_pos : Vec3, b_pos : Vec3, ball_size : f32) {
-//     if (a_pos - b_pos).length() <= 2 * ball_size {
-
-//     }
-// }
