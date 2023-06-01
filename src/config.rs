e57371de172e7fa502632b4ad228f9271d3c488a
AlexBrodbelt <apbrodbelt@gmail.com>
2023-06-01 18:25:11 +0100

    added some cue stick constants

--- src/config.rs
+++ src/config.rs
@@ -12,15 +12,10 @@ pub const TABLE_HEIGHT: f32 = 600.0;
 pub const TABLE_WIDTH: f32 = 1200.0;
 // These pub constants are defined in `Transform` units.
 // Using the default 2D camera they correspond 1:1 with screen pixels.
-pub const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
-pub const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
+pub const CUESTICK_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
 pub const PADDLE_SPEED: f32 = 500.0;
-// How close can the paddle get to the wall
-pub const PADDLE_PADDING: f32 = 10.0;
 
-// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
-pub const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
-pub const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+
 pub const BALL_RADIUS: f32 = TABLE_WIDTH / 136.0;
 // Pocket
 pub const POCKET_RADIUS: f32 = 20.0;
@@ -47,9 +42,7 @@ pub const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 pub const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 
 pub const BACKGROUND_COLOR: Color = Color::rgb(0.04, 0.42, 0.01);
-pub const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-pub const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
-pub const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+pub const CUE_STICK_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 pub const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 pub const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 pub const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
