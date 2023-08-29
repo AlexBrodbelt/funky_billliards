bb0cebb1268f72201a15c9b58d7dcce92db1076c
AlexBrodbelt <apbrodbelt@gmail.com>
2023-08-29 12:54:14 +0100

    working on server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
 use log::{info, trace, warn};
+use renet::{ChannelConfig, SendType};
+use renet::transport::NetcodeServerTransport;
 use renet::{
-    RenetConnectionConfig, RenetServer, ServerAuthentication, ServerConfig, ServerEvent,
+    ConnectionConfig, RenetServer, transport::ServerAuthentication, transport::ServerConfig, ServerEvent,
     NETCODE_USER_DATA_BYTES,
 };
 use std::net::{SocketAddr, UdpSocket};
@@ -133,4 +135,101 @@ fn main() {
         server.send_packets().unwrap();
         thread::sleep(Duration::from_millis(50));
     }
+}
+
+pub enum ClientChannel {
+    Input,
+    Command,
+}
+
+pub enum ServerChannel {
+    ServerMessages,
+    NetworkedEntities,
+}
+
+impl From<ClientChannel> for u8 {
+    fn from(channel_id: ClientChannel) -> Self {
+        match channel_id {
+            ClientChannel::Command => 0,
+            ClientChannel::Input => 1,
+        }
+    }
+}
+
+impl ClientChannel {
+    pub fn channels_config() -> Vec<ChannelConfig> {
+        vec![
+            ChannelConfig {
+                channel_id: Self::Input.into(),
+                max_memory_usage_bytes: 5 * 1024 * 1024,
+                send_type: SendType::ReliableOrdered {
+                    resend_time: Duration::ZERO,
+                },
+            },
+            ChannelConfig {
+                channel_id: Self::Command.into(),
+                max_memory_usage_bytes: 5 * 1024 * 1024,
+                send_type: SendType::ReliableOrdered {
+                    resend_time: Duration::ZERO,
+                },
+            },
+        ]
+    }
+}
+
+impl From<ServerChannel> for u8 {
+    fn from(channel_id: ServerChannel) -> Self {
+        match channel_id {
+            ServerChannel::NetworkedEntities => 0,
+            ServerChannel::ServerMessages => 1,
+        }
+    }
+}
+
+impl ServerChannel {
+    pub fn channels_config() -> Vec<ChannelConfig> {
+        vec![
+            ChannelConfig {
+                channel_id: Self::NetworkedEntities.into(),
+                max_memory_usage_bytes: 10 * 1024 * 1024,
+                send_type: SendType::Unreliable,
+            },
+            ChannelConfig {
+                channel_id: Self::ServerMessages.into(),
+                max_memory_usage_bytes: 10 * 1024 * 1024,
+                send_type: SendType::ReliableOrdered {
+                    resend_time: Duration::from_millis(200),
+                },
+            },
+        ]
+    }
+}
+
+
+pub fn connection_config() -> ConnectionConfig {
+    ConnectionConfig {
+        available_bytes_per_tick: 1024 * 1024,
+        client_channels_config: ClientChannel::channels_config(),
+        server_channels_config: ServerChannel::channels_config(),
+    }
+}
+
+fn new_renet_server() -> (RenetServer, NetcodeServerTransport) {
+    let server = RenetServer::new(connection_config());
+
+    let public_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(public_addr).unwrap();
+    let current_time: std::time::Duration = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let server_config = ServerConfig {
+        // current_time,
+        max_clients: 64,
+        protocol_id: PROTOCOL_ID,
+        // public_addresses: vec![public_addr],
+        authentication: ServerAuthentication::Unsecure,
+        public_addr,
+    };
+
+    let transport = NetcodeServerTransport::new(current_time, server_config, socket).unwrap();
+
+    (server, transport)
 }
